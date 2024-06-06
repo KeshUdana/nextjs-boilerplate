92bbdbb145cbc38d80c606135f60328c550d04fd
Keshawa_Udana <125798571+KeshUdana@users.noreply.github.com>
2024-06-06 08:56:55 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Homepage from "./pages/homepage"
 import Education from "./pages/education"
+import ForgeLab from "./pages/forgelab"
 
 
 export default function Home() {
@@ -11,6 +12,9 @@ export default function Home() {
       </section>
       <section id="education">
         <Education/>
+      </section>
+      <section>
+        <ForgeLab/>
       </section>
     </>
   );
