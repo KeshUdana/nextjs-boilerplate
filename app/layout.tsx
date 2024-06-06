eaecba0282784e260d9c1d890cac9229c95b5cf8
Keshawa_Udana <125798571+KeshUdana@users.noreply.github.com>
2024-06-06 09:40:46 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,10 @@ export default function RootLayout({
         <main>
           <nav className="fixed w-screen px-4 py-4 flex justify-between items-center bg-stone-950 text-white font-mono z-40">
             <ul className="flex justify-between w-full px-3 text-white">
-              <li className=" hover:text-blue-500"><Link href="#Homepage">Homepage</Link></li>
-              <li className=" hover:text-green-500"><Link href="#Education">Education</Link></li>
-              <li className=" hover:text-orange-500"><Link href="#ForgeLab">Forge Lab</Link></li>
-              <li className=" hover:text-purple-500"><Link href="#Modde">Modde</Link></li>
+              <li className=" hover:text-blue-500"><Link href="#homepage">Homepage</Link></li>
+              <li className=" hover:text-green-500"><Link href="#education">Education</Link></li>
+              <li className=" hover:text-orange-500"><Link href="#forgelab">Forge Lab</Link></li>
+              <li className=" hover:text-purple-500"><Link href="#modde">Modde</Link></li>
             </ul>
           </nav>
         {children}</main></body>
