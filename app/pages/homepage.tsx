fa87295e6edbc93b37989180ca6a404534475401
Keshawa_Udana <125798571+KeshUdana@users.noreply.github.com>
2024-06-06 09:22:11 +0530

    Update homepage.tsx

--- app/pages/homepage.tsx
+++ app/pages/homepage.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 function Homepage() {
     return (
-        <> <section className=" bg-stone-900 h-screen ">
+        <> <section className="inset-0 bg-gradient-to-r from-charcoal-grey via-space-blue to-charcoal-grey h-screen ">
             <ul className="flex-col  py-12 px-9">
                 <li className="font-Nunito text-9xl font-light text-white decoration-double px-50% py-10">HELLO.</li>
                 <li className="font-Roboto text-4xl font-normal text-lime-500 ">Keshawa Udana</li>
