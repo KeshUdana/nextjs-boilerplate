21003c832cd539bbd8f128d438aaf66d24661728
Keshawa_Udana <125798571+KeshUdana@users.noreply.github.com>
2024-06-07 18:07:06 +0530

    Create projects.tsx

---
+++ app/pages/projects.tsx
@@ -0,0 +1,44 @@
+
+import React from "react";
+import '../globals.css';
+
+function Projects(){
+    return(
+        <>
+            <section className="h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
+                <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Train ticket booking system</h5>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem Ipsum fhghghghghghg jjjdksks wkdadadnwowkcak </p>
+                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    See more</a>
+                </div>
+
+                <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Race Betting platform</h5>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem Ipsum fhghghghghghg jjjdksks wkdadadnwowkcak </p>
+                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    See more</a>
+                </div>
+
+                <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Project 3</h5>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem Ipsum fhghghghghghg jjjdksks wkdadadnwowkcak </p>
+                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    See more</a>
+                </div>
+
+                <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Project 4</h5>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem Ipsum fhghghghghghg jjjdksks wkdadadnwowkcak </p>
+                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    See more</a>
+                </div>
+                
+
+
+            </section>
+        </>
+    )
+
+};
+export default Projects;
