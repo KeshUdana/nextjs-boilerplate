cb9d5843542a2a5722fd7b587e40bb9ac77270fe
Keshawa_Udana <125798571+KeshUdana@users.noreply.github.com>
2024-06-14 04:51:00 +0000

    av

--- app/pages/modde.tsx
+++ app/pages/modde.tsx
@@ -8,7 +8,7 @@ function Modde(){
         <div className="container mx-auto px-4 py-8">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-6 mt-10">
             <div className="max-w-lg p-6 bg-white/20 rounded-lg items-center md:w-screen">
-            <h5 className="mb-2 text-7xl text-center font-bold tracking-tight text-purple-700 dark:text-pink md:text-9xl md:mt-10">MODDE</h5>
+            <h5 className="mb-2 text-7xl text-center font-bold tracking-tight text-purple-700 dark:text-pink md:text-9xl md:mt-10 md:font-bold">MODDE</h5>
                 <p className="text-xl text-white text-center">Discover Your Style with AI</p>
                             <p className="mb-3 font-bold text-stone-800 text-center"><br/>
                             Experience cutting-edge fashion technology on our AI-powered web platform. 
