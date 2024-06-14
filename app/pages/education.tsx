621a3a6164e220c084137abe8494a51f5ff524f3
Keshawa_Udana <125798571+KeshUdana@users.noreply.github.com>
2024-06-14 05:01:17 +0000

    sss

--- app/pages/education.tsx
+++ app/pages/education.tsx
@@ -15,7 +15,7 @@ function Education(){
                 </div>
                 <p className="font-mono text-xl text-fuchsia-700"> TECH STACK</p>
                 <div className="flex flex-col items-center container mx-auto px-10 py-4">
-  <div className="grid sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-8 gap-2">
+  <div className="grid grid-cols-8 gap-5">
     <Image src="/python-svgrepo-com (1).svg" alt="python" width={50} height={24}/>
     <Image src="/java-4-logo-svgrepo-com (1).svg" alt="java" width={50} height={24}/>
     <Image src="/javascript-svgrepo-com (1).svg" alt="javascript" width={50} height={24}/>
@@ -36,8 +36,8 @@ function Education(){
     <Image src="/numpy-svgrepo-com (1).svg" alt="numpy" width={50} height={24}/>
     <Image src="/pandas-svgrepo-com (1).svg" alt="pandas" width={50} height={24}/>
   </div>
-  <ol className="text-center flex flex-col items-center mt-4">
-    <li className="text-xl text-zinc-400">GCSE.Biological Sciences for Advanced Levels at Rahula College Matara</li>
+  <ol className="text-center flex flex-col items-center mt-4 ">
+    <li className="text-xl text-zinc-400">GCSE.Advanced Levels at Rahula College Matara</li>
     <Image src="/rahula-college-seeklogo-2 (1).svg" alt="pandas" width={60} height={24}/>
   </ol>
 </div>
